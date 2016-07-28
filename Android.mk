919ff44fa2505ebcc2cf91ea1d01386151f4247b
grace5921 <androidlover5842@gmail.com>
2016-07-28 19:48:45 +0530

    initial commit

---
+++ Android.mk
@@ -0,0 +1,12 @@
+#Enables the listed display HAL modules
+#libs to be built for QCOM targets only
+
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
+
+display-hals := libgralloc libgenlock libcopybit
+display-hals += libhwcomposer liboverlay libqdutils
+display-hals += libtilerenderer
+
+include $(call all-named-subdir-makefiles,$(display-hals))
+
+endif
